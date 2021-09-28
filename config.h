562d52effdd980ba357ce914d9ab315e9a28c17e
tiwari-krishna <89651392+tiwari-krishna@users.noreply.github.com>
2021-09-28 07:23:56 +0545

    Add files via upload

--- config.h
+++ config.h
@@ -2,26 +2,23 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Ubuntu:size=10",
-    "JoyPixels:size=10"
+	"Cantarell:size=10",
+    "Noto Color Emoji:size=9"
 };
-static const char *prompt      = {"Run:"};      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#0d83b5" },
-    [SchemeSelHighlight] = { "#d7d7d7", "#000000" },
-    [SchemeNormHighlight] = { "#a5bdf8", "#000000" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-    [SchemeOutHighlight] = { "#d7d7d7", "#1f2026" },
-    [SchemeHp] = { "#bbbbbb", "#333333" }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -29,7 +26,5 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 
-/*
- * Use prefix matching by default; can be inverted with the -x flag.
- */
-static int use_prefix = 1;
+/* Size of the window border */
+static const unsigned int border_width = 0;
