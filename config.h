9bc7a4b0bab700975ab79089c19b21ddeb5c0aac
tiwari-krishna <89651392+tiwari-krishna@users.noreply.github.com>
2021-11-25 08:38:41 +0545

    Add files via upload

--- config.h
+++ config.h
@@ -6,15 +6,15 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Roboto:size=11",
-    "Noto Color Emoji:size=11"
+	"Ubuntu:size=11",
+    "Noto Color Emoji:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#ebdbb2", "#98971a" },
-    [SchemeMid] = { "#eeeeee", "#770000" },
+	[SchemeSel] = { "#ebdbb2", "#4965a6" },
+    [SchemeMid] = { "#eeeeee", "#170101" },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
