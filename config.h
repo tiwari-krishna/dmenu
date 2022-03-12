0cdb9105f7951c678333a0e99115d825b258ab9f
Krishna Tiwari <krishna@artix.localdomain>
2022-03-12 12:17:00 +0545

    Minor Commit

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Monospace:pixelsize=14:antialias=true:autohint=true",
+	"Monospace:pixelsize=15:antialias=true:autohint=true",
     "Noto Color Emoji:pixelsize=13:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
